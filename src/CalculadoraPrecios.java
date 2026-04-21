6a441aa891c61a67afbb38925f208efd9e0ebc5d
azamatkhu <azamat.khu@educa.jcyl.es>
2026-04-21 13:19:14 +0200

    Bulk upload

--- src/CalculadoraPrecios.java
+++ src/CalculadoraPrecios.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class CalculadoraPrecios {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Dime un precio base");
+        double precioBase = sc.nextDouble();
+
+        sc.nextLine();
+
+        System.out.println("Dime un tipo de cliente");
+        String tipoCliente = sc.nextLine();
+
+        double descuentoAplicado = 0.0;
+
+        switch (tipoCliente){
+            case "estudiante":
+                descuentoAplicado = ConstantesDescuento.descuentoEstudiante;
+                break;
+            case "mayor":
+                descuentoAplicado = ConstantesDescuento.descuentoMayor;
+                break;
+            case "vip":
+                descuentoAplicado = ConstantesDescuento.descuentoVip;
+                break;
+            default:
+                System.out.println("Error: tipo de cliente invalido");
+                break;
+        }
+
+        double precioDescontado = precioBase - (descuentoAplicado * precioBase);
+        double precioFinal = precioBase + (precioDescontado * ConstantesDescuento.iva);
+
+        System.out.println("Precio final: " + precioFinal);
+
+        sc.close();
+    }
 }
--- src/ConstantesDescuento.java
+++ src/ConstantesDescuento.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class ConstantesDescuento {
+    public static final double descuentoEstudiante = 0.20;
+    public static final double descuentoMayor = 0.30;
+    public static final double descuentoVip = 0.15;
+    public static final double iva = 0.21;
 }
